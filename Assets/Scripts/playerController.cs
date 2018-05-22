b08e08d4adebaed32a7064aa1daf37dd8c77308c
Bhavin Padhiar <bhavinpadhiar@Bhavins-MacBook-Pro.local>
2018-05-22 19:38:11 +0100

    Added gitignore and unity files

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	//movement variables
+	public float maxSpeed;
+
+	Rigidbody2D myRb;
+	Animator myAnim;
+	bool facingRight;
+
+	// Use this for initialization
+	void Start () {
+		myRb = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
+		facingRight = true;
+	}
+	
+	// Update is called once per frame. FixedUpdate is called exactly at same specified time interval
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+		myAnim.SetFloat ("speed", Mathf.Abs (move));
+
+		myRb.velocity = new Vector2 (move * maxSpeed, myRb.velocity.y);
+
+		if(move > 0 && !facingRight) {
+			flip ();
+		} else if (move < 0 && facingRight) {
+			flip ();
+		}
+	}
+
+	void flip() {
+
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+}
